31bc74f32f59affc2da0c1c403ed4a4a34ce3eb1
gyojincompany <luckyred@gmail.com>
2023-02-09 16:45:11 +0900

    Validator 연습

---
+++ src/main/java/com/gyojincompany/home/LoginValidator.java
@@ -0,0 +1,34 @@
+package com.gyojincompany.home;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class LoginValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> clazz) {//검증할 객체(memberDto) 클래스 타입 정보
+		// TODO Auto-generated method stub
+		return MemberDto.class.isAssignableFrom(clazz);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		
+		MemberDto memberDto = (MemberDto) target;
+		//검증하려는 객체인 memberDto 객체를 검증 타겟으로 설정
+		String memberId = memberDto.getId();
+		int memberPw = memberDto.getPw();
+		
+		if(memberId == null || memberId.trim().isEmpty()) {
+			errors.rejectValue("id", "trouble");
+		}
+		
+		if(memberPw == 0) {
+			errors.rejectValue("pw", "trouble");
+		}
+		
+
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/loginOk.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	[로그인 성공]<br>
+	아이디 : ${memberDto.id }<br>
+	비밀번호 : ${memberDto.pw }
+</body>
+</html>
